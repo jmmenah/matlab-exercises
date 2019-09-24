f44eec668f6ba9c20e624d22a2dfe21a28591614
jmmenah <55758232+jmmenah@users.noreply.github.com>
2019-09-25 00:05:22 +0200

    Add files via upload

---
+++ Scripts/Seminario Arduino/destellos.m
@@ -0,0 +1,49 @@
+%Script destellos. Programa que pide un número de destellos y enciende
+% alternativamente los LEDs 13 y 9 dicho número de veces durante un segundo.
+% Finalmente, se apagan los LEDs
+
+%Lectura de datos
+num = input('Introduzca un número: '); % número de destellos en cada Led
+ 
+pinLed = [9 10 11 12 13];
+i = 1;
+
+for cont = 1:num
+    mi_arduino.digitalWrite(pinLed(1),1);
+    mi_arduino.digitalWrite(pinLed(2),0);
+    mi_arduino.digitalWrite(pinLed(3),0);
+    mi_arduino.digitalWrite(pinLed(4),0);
+    mi_arduino.digitalWrite(pinLed(5),0);
+    pause(0.5); %Para la ejecución un segundo 
+    mi_arduino.digitalWrite(pinLed(1),0);
+    mi_arduino.digitalWrite(pinLed(2),1);
+    mi_arduino.digitalWrite(pinLed(3),0);
+    mi_arduino.digitalWrite(pinLed(4),0);
+    mi_arduino.digitalWrite(pinLed(5),0);
+    pause(0.5); %Para la ejecución un segundo
+    mi_arduino.digitalWrite(pinLed(1),0);
+    mi_arduino.digitalWrite(pinLed(2),0);
+    mi_arduino.digitalWrite(pinLed(3),1);
+    mi_arduino.digitalWrite(pinLed(4),0);
+    mi_arduino.digitalWrite(pinLed(5),0);
+    pause(0.5); %Para la ejecución un segundo
+    mi_arduino.digitalWrite(pinLed(1),0);
+    mi_arduino.digitalWrite(pinLed(2),0);
+    mi_arduino.digitalWrite(pinLed(3),0);
+    mi_arduino.digitalWrite(pinLed(4),1);
+    mi_arduino.digitalWrite(pinLed(5),0);
+    pause(0.5); %Para la ejecución un segundo
+    mi_arduino.digitalWrite(pinLed(1),0);
+    mi_arduino.digitalWrite(pinLed(2),0);
+    mi_arduino.digitalWrite(pinLed(3),0);
+    mi_arduino.digitalWrite(pinLed(4),0);
+    mi_arduino.digitalWrite(pinLed(5),1);
+    pause(0.5); %Para la ejecución un segundo
+    
+end
+ 
+mi_arduino.digitalWrite(pinLed(1),0);
+mi_arduino.digitalWrite(pinLed(2),0);
+mi_arduino.digitalWrite(pinLed(3),0);
+mi_arduino.digitalWrite(pinLed(4),0);
+mi_arduino.digitalWrite(pinLed(5),0);
---
+++ Scripts/Seminario Arduino/inicializa_pines_leds.m
@@ -0,0 +1,5 @@
+mi_arduino.pinMode(9,'output');
+mi_arduino.pinMode(10,'output');
+mi_arduino.pinMode(11,'output');
+mi_arduino.pinMode(12,'output');	
+mi_arduino.pinMode(13,'output');
